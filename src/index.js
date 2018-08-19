c7919f5be15b4fa41c8fcd271fbf1726bad86e5e
Vlad Trebukhov <vladtrebukhov123@gmail.com>
2018-08-19 14:11:22 -0400

    stable build

--- src/index.js
+++ src/index.js
@@ -7,9 +7,11 @@ import StorePicker from "./components/storepicker.js";
 import NotFound from "./components/notfound.js";
 import App from "./app.js";
 
+const repo = `/${window.location.pathname.split("/")[1]}`;
+
 const Root = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={repo}>
       <div>
         <Match exactly pattern="/" component={StorePicker} />
         <Match pattern="/store/:storeId" component={App} />
