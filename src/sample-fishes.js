89499b7ca32472a0af832e0d27934117b3d05655
Vlad Trebukhov <vladtrebukhov123@gmail.com>
2018-09-14 15:07:59 -0400

    edited info

--- src/sample-fishes.js
+++ src/sample-fishes.js
@@ -1,74 +1,90 @@
 // This is just some sample data so you don't have to think of your own!
 module.exports = {
   fish1: {
-    name: 'Pacific Halibut',
-    image: 'http://i.istockimg.com/file_thumbview_approve/36248396/5/stock-photo-36248396-blackened-cajun-sea-bass.jpg',
-    desc: 'Everyones favorite white fish. We will cut it to the size you need and ship it.',
+    name: "Pacific Halibut",
+    image:
+      "https://www.santamonicaseafood.com/wp-content/uploads/2016/02/Pacific-Halibut-Cooked.jpeg",
+    desc:
+      "Everyones favorite white fish. We will cut it to the size you need and ship it.",
     price: 1724,
-    status: 'available'
+    status: "available"
   },
 
   fish2: {
-    name: 'Lobster',
-    image: 'http://i.istockimg.com/file_thumbview_approve/32135274/5/stock-photo-32135274-cooked-lobster.jpg',
-    desc: 'These tender, mouth-watering beauties are a fantastic hit at any dinner party.',
+    name: "Lobster",
+    image:
+      "https://img.grouponcdn.com/iam/mADQWmRmj4dHZt7Nw27r/mF-2048x1242/v1/c700x420.jpg",
+    desc:
+      "These tender, mouth-watering beauties are a fantastic hit at any dinner party.",
     price: 3200,
-    status: 'available'
+    status: "available"
   },
 
   fish3: {
-    name: 'Sea Scallops',
-    image: 'http://i.istockimg.com/file_thumbview_approve/58624176/5/stock-photo-58624176-scallops-on-black-stone-plate.jpg',
-    desc: 'Big, sweet and tender. True dry-pack scallops from the icey waters of Alaska. About 8-10 per pound',
+    name: "Sea Scallops",
+    image:
+      "https://cdn.lifesambrosia.com/wp-content/uploads/seared-sea-scallops.jpg",
+    desc:
+      "Big, sweet and tender. True dry-pack scallops from the icey waters of Alaska. About 8-10 per pound",
     price: 1684,
-    status: 'unavailable'
+    status: "unavailable"
   },
 
   fish4: {
-    name: 'Mahi Mahi',
-    image: 'http://i.istockimg.com/file_thumbview_approve/12556651/5/stock-photo-12556651-mahimahi.jpg',
-    desc: 'Lean flesh with a mild, sweet flavor profile, moderately firm texture and large, moist flakes. ',
+    name: "Mahi Mahi",
+    image:
+      "https://d39ziaow49lrgk.cloudfront.net/wp-content/uploads/2016/07/Mahi-Mahi-with-Mango-Jalapeno-Salsa-Recipe744.jpg?x88542",
+    desc:
+      "Lean flesh with a mild, sweet flavor profile, moderately firm texture and large, moist flakes. ",
     price: 1129,
-    status: 'available'
+    status: "available"
   },
 
   fish5: {
-    name: 'King Crab',
-    image: 'http://i.istockimg.com/file_thumbview_approve/18294110/5/stock-photo-18294110-king-crab-legs.jpg',
-    desc: 'Crack these open and enjoy them plain or with one of our cocktail sauces',
+    name: "King Crab",
+    image:
+      "https://cmt.azureedge.net/media/how_to_cook_perfect_king_crab_legs_whats_20150125231555272604y93yqs.jpg",
+    desc:
+      "Crack these open and enjoy them plain or with one of our cocktail sauces",
     price: 4234,
-    status: 'available'
+    status: "available"
   },
 
   fish6: {
-    name: 'Atlantic Salmon',
-    image: 'http://i.istockimg.com/file_thumbview_approve/56241842/5/stock-photo-56241842-salmon-fish.jpg',
-    desc: 'This flaky, oily salmon is truly the king of the sea. Bake it, grill it, broil it...as good as it gets!',
+    name: "Atlantic Salmon",
+    image:
+      "http://www.besthealthmag.ca/wp-content/uploads/2016/01/health-benefits-salmon.jpg",
+    desc:
+      "This flaky, oily salmon is truly the king of the sea. Bake it, grill it, broil it...as good as it gets!",
     price: 1453,
-    status: 'available'
+    status: "available"
   },
 
   fish7: {
-    name: 'Oysters',
-    image: 'http://i.istockimg.com/file_thumbview_approve/58626682/5/stock-photo-58626682-fresh-oysters-on-a-black-stone-plate-top-view.jpg',
-    desc: 'A soft plump oyster with a sweet salty flavor and a clean finish.',
+    name: "Oysters",
+    image:
+      "https://cdn-image.myrecipes.com/sites/default/files/styles/medium_2x/public/image/recipes/ck/01/11/baked-oysters-ck-237772-x.jpg?itok=kyxcehHT",
+    desc: "A soft plump oyster with a sweet salty flavor and a clean finish.",
     price: 2543,
-    status: 'available'
+    status: "available"
   },
 
   fish8: {
-    name: 'Mussels',
-    image: 'http://i.istockimg.com/file_thumbview_approve/40450406/5/stock-photo-40450406-steamed-mussels.jpg',
-    desc: 'The best mussels from the Pacific Northwest with a full-flavored and complex taste.',
+    name: "Mussels",
+    image:
+      "https://assets.marthastewart.com/styles/wmax-1500/d45/steamed-mussels-with-wine-and-saffron-mscs108/steamed-mussels-with-wine-and-saffron-mscs108_horiz.jpg?itok=5MDf8WDC",
+    desc:
+      "The best mussels from the Pacific Northwest with a full-flavored and complex taste.",
     price: 425,
-    status: 'available'
+    status: "available"
   },
 
   fish9: {
-    name: 'Jumbo Prawns',
-    image: 'http://i.istockimg.com/file_thumbview_approve/67121439/5/stock-photo-67121439-fresh-tiger-shrimp-on-ice-on-a-black-stone-table.jpg',
-    desc: 'With 21-25 two bite prawns in each pound, these sweet morsels are perfect for shish-kabobs.',
+    name: "Jumbo Shrimp",
+    image: "https://i.ytimg.com/vi/3W0UF4ZCpeg/maxresdefault.jpg",
+    desc:
+      "With 21-25 two bite shrimp in each pound, these sweet morsels are perfect for shish-kabobs.",
     price: 2250,
-    status: 'available'
+    status: "available"
   }
 };
